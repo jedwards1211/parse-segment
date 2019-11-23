edaa73e46c27ebd92f132e2c15ac5bfbcdce2324
Andy Edwards <jedwards@fastmail.com>
2019-11-23 15:31:11 -0600

    fix: add missing index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+import Segment from './Segment'
+import SegmentParseError from './SegmentParseError'
+import SegmentParser from './SegmentParser'
+
+export { Segment, SegmentParseError, SegmentParser }
